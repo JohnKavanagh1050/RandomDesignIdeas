a70a334664f3e8c347ecf3c42e638e5f0c3c61fa
johnkavanagh1050 <johnkav1050@gmail.com>
2018-12-13 17:07:38 +0000

    pushing to work on at home

---
+++ app/src/main/java/com/randomgimmicks/jkavanagh/randomgimmicks/MainActivity.java
@@ -0,0 +1,43 @@
+package com.randomgimmicks.jkavanagh.randomgimmicks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String url = "http://192.168.43.196/Volley/data.json";
+
+    private RecyclerView mList;
+
+    private LinearLayoutManager linearLayoutManager;
+    private DividerItemDecoration dividerItemDecoration;
+    private List<Movie> movieList;
+    private RecyclerView.Adapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mList = findViewById(R.id.main_list);
+
+        movieList = new ArrayList<>();
+        adapter = new MovieAdapter(getApplicationContext(),movieList);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
+
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(linearLayoutManager);
+        mList.addItemDecoration(dividerItemDecoration);
+        mList.setAdapter(adapter);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
